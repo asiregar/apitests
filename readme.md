5f2a6c125194ff743a10fca127cadcbe27aad4b1
asiregar <asiregar@spartaglobal.co>
2016-11-22 12:24:34 +0000

    demo ready

---
+++ readme.md
@@ -0,0 +1,10 @@
+#HTTParty Todos API Tester
+
+This is a series of tests attempting to perform various HTTP requests on the Todos API on lacedeamon.spartaglobal
+to run the tests please run 'rspec' in the terminal in the apitests directory
+
+###Contributors
+
+BABEraham
+Test Harnish
+Super Ash Bros
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,91 +1,94 @@
 require 'HTTParty'
-require 'pry'
 
-describe HTTParty do
+describe "The Todos API" do
 posted_ids=[] #creating an array containing the IDs of all ToDos posted to the url by this test for clearup later
-test_url="http://lacedeamon.spartaglobal.com/todos"
-
+ 
   it "should POST to Todos" do
-    post = HTTParty.post(test_url, query:{title: 'new toDO', due: Date.parse("1 Jan 2009")})
-    posted_ids.push(post['id'])
-    expect(post.message).to eq 'Created'
-    expect(post.code).to eq 201
+    posted = HTTParty.post url('/todos'), query:{title: 'new toDO', due: Date.parse("1 Jan 2009")}
+    posted_ids.push(posted['id'])
+    expect(posted.message).to eq 'Created'
+    expect(posted.code).to eq 201
+  end
+
+  it "should only POST if the necessary arguments have been supplied" do
+    invalid_post = HTTParty.post url('/todos'), query:{title: 'new toDO'}
+    expect(invalid_post.message).to eq 'Unprocessable Entity'
+    expect(invalid_post.code).to eq 422
   end
 
   it "shouldn't be allowed to post to a specific todo" do
-    post = HTTParty.post(test_url, query:{title: 'good toDO', due: Date.parse("3 Jan 2009")})
-    posted_ids.push(post['id'])
-    invalid_post = HTTParty.post("#{test_url}/#{post['id']}", query:{title: 'bad toDO', due: Date.parse("12 Jan 2019")})
+    posted = HTTParty.post url('/todos'), query:{title: 'good toDO', due: Date.parse("3 Jan 2009")}
+    posted_ids.push posted['id']
+    invalid_post = HTTParty.post url("/todos/#{posted['id']}"), query:{title: 'bad toDO', due: Date.parse("12 Jan 2019")}
     expect(invalid_post.message).to eq 'Method Not Allowed'
     expect(invalid_post.code).to eq 405
   end
 
   it "should DELETE a Todo" do
-    post = HTTParty.post(test_url, query:{title: 'delete me bitch', due: Date.today})
-    p1id = post["id"]
-    del = HTTParty.delete("#{test_url}/#{p1id}")
+    posted = HTTParty.post url('/todos'), query:{title: 'delete me bitch', due: Date.today}
+    del = HTTParty.delete url("/todos/#{posted['id']}")
     expect(del.message).to eq 'No Content'
     expect(del.code).to eq 204
   end
 
   it "should not be able to delete the entire collection" do
-    del = HTTParty.delete(test_url)
+    del = HTTParty.delete url('/todos')
     expect(del.message).to eq 'Method Not Allowed'
     expect(del.code).to eq 405
   end
 
   it "should return 200 for successful GET requests on the collection" do
-    get = HTTParty.get test_url
-    expect(get.code).to eq 200
-    expect(get.message).to eq 'OK'
+    get_req = HTTParty.get url('/todos')
+    expect(get_req.code).to eq 200
+    expect(get_req.message).to eq 'OK'
   end
 
   it "should GET a specific Todo by its ID" do
-    post = HTTParty.post(test_url, query:{title: 'find me!', due: Date.parse("1 Jan 2009")})
-    posted_ids.push(post['id'])
-    get = HTTParty.get "#{test_url}/#{post['id']}"
-    expect(get.code).to eq 200
-    expect(get.message).to eq 'OK'
-    expect(get["title"]).to eq "find me!"
+    posted = HTTParty.post url('/todos'), query:{title: 'find me!', due: Date.parse("1 Jan 2009")}
+    posted_ids.push posted['id']
+    get_req = HTTParty.get url("/todos/#{posted['id']}")
+    expect(get_req.code).to eq 200
+    expect(get_req.message).to eq 'OK'
+    expect(get_req["title"]).to eq "find me!"
   end
 
   it "should be able to patch an existing Todo's title" do
-    post = HTTParty.post(test_url, query:{title: 'patch me!', due: Date.today})
-    posted_ids.push(post['id'])
-    patchtitle = HTTParty.patch("#{test_url}/#{post['id']}", query:{title: 'patched'})
-    patchdate = HTTParty.patch("#{test_url}/#{post['id']}", query:{due: Date.parse("02 Feb 2020")})
-    get = HTTParty.get("#{test_url}/#{post['id']}")
-    expect(get['title']).to eq "patched"
-    expect(get['due']).to eq "2016-11-21"
-    expect(get.code).to eq 200
-    expect(get.message).to eq 'OK'
+    posted = HTTParty.post url('/todos'), query:{title: 'patch me!', due: Date.parse("21 Nov 2016")}
+    posted_ids.push posted['id']
+    patchtitle = HTTParty.patch url("/todos/#{posted['id']}"), query:{title: 'patched'}
+    patchdate = HTTParty.patch url("/todos/#{posted['id']}"), query:{due: Date.parse("02 Feb 2020")}
+    get_req = HTTParty.get url("/todos/#{posted['id']}")
+    expect(get_req['title']).to eq "patched"
+    expect(get_req['due']).to eq "2020-02-02"
+    expect(get_req.code).to eq 200
+    expect(get_req.message).to eq 'OK'
   end
 
   it "shouldn't be able to patch an entire collection" do
-    patch = HTTParty.patch(test_url, query:{title: 'patched', due: Date.parse("02 Feb 2020")})
+    patch = HTTParty.patch url('/todos'), query:{title: 'patched', due: Date.parse("02 Feb 2020")}
     expect(patch.code).to eq 405
     expect(patch.message).to eq 'Method Not Allowed'
   end
 
   it "it should be able to run PUT requests" do
-    post = HTTParty.post(test_url, query:{title: 'put meh!', due: Date.today})
-    posted_ids.push(post['id'])
-    put = HTTParty.put("#{test_url}/#{post['id']}", query:{title: 'get putted', due: Date.parse("20 April 2420")})
-    get = HTTParty.get("#{test_url}/#{post['id']}")
+    posted = HTTParty.post url('/todos'), query:{title: 'put meh!', due: Date.today}
+    posted_ids.push posted['id']
+    put = HTTParty.put url("/todos/#{posted['id']}"), query:{title: 'get putted', due: Date.parse("20 April 2420")}
+    get = HTTParty.get url("/todos/#{posted['id']}")
     expect(get['title']).to eq 'get putted'
     expect(get.code).to eq 200
     expect(get.message).to eq "OK"
   end
 
   it "shouldn't be able to PUT to the collection" do
-    put = HTTParty.put(test_url, query:{title: 'invalid put', due: Date.parse("20 April 2420")})
+    put = HTTParty.put url('/todos'), query:{title: 'invalid put', due: Date.parse("20 April 2420")}
     expect(put.code).to eq 405
     expect(put.message).to eq 'Method Not Allowed'
   end
 
-  it "should tear down after testing" do
+  def teardown
     posted_ids.each do |id|
-      del = HTTParty.delete("#{test_url}/#{id}")
+      del = HTTParty.delete url("/todos/#{id}")
       expect(del.message).to eq 'No Content'
       expect(del.code).to eq 204
     end
